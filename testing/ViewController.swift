1d83c3d90e004cc65070c89031407bcd585f427b
AHKNT <nf700v1010@gmail.com>
2018-09-20 15:46:19 +0900

    Initial Commit

---
+++ testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by 大家拓 on 2018/09/20.
+//  Copyright © 2018年 大家拓. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
